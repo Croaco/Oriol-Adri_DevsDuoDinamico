a52f0c22041c9c33e1f5b57a070b511793a3c613
Croaco <abelmontevall@hotmail.com>
2022-11-03 15:26:20 +0100

    Probando cositas del player

--- Game/Source/Player.cpp
+++ Game/Source/Player.cpp
@@ -58,22 +58,25 @@ bool Player::Update()
 	// L07 DONE 5: Add physics to the player - updated player position using physics
 
 	int speed = 10; 
-	b2Vec2 vel = b2Vec2(0, -GRAVITY_Y); 
+	b2Vec2 vel = b2Vec2(0, -GRAVITY_Y/2);
 
 	//L02: DONE 4: modify the position of the player using arrow keys and render the texture
 	if (app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) {
-		//
+		vel = b2Vec2(0, GRAVITY_Y);
+	}
+	if (app->input->GetKey(SDL_SCANCODE_SPACE) == KEY_REPEAT) {
+		vel = b2Vec2(0, GRAVITY_Y);
 	}
 	if (app->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) {
 		//
 	}
 		
 	if (app->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
-		vel = b2Vec2(-speed, -GRAVITY_Y);
+		vel = b2Vec2(-speed, -GRAVITY_Y/2);
 	}
 
 	if (app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
-		vel = b2Vec2(speed, -GRAVITY_Y);
+		vel = b2Vec2(speed, -GRAVITY_Y/2);
 	}
 
 	//Set the velocity of the pbody of the player
